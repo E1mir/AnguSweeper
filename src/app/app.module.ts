6fe80f5e330ddcf5fa9252ea1256bd96dfb43ef5
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-17 16:52:14 +0400

    [project] Register all new created files to module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameBoardComponent } from './game-board/game-board.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GameComponent } from './game/game.component';
+import { ConvertSecondsToTimePipe } from './pipes/convert-seconds-to-time.pipe';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameBoardComponent
+    GameBoardComponent,
+    HomeComponent,
+    GameComponent,
+    ConvertSecondsToTimePipe,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
