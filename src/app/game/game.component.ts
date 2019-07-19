c23b1b378ae2eb044e5bfda832c4dbbedae4273a
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-19 11:57:27 +0400

    [refact] Update game component connect it with game service

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,41 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { EASY, HARD, MEDIUM } from '../app.constants';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ADVANCED, BEGINNER, EXPERT, INTERMEDIATE } from '../app.constants';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { GameService } from '../services/game.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
 
-  public timer: number = 0;
+  public time: number = 0;
   public selectDifficultFormGroup: FormGroup;
-  public EASY = EASY;
-  public MEDIUM = MEDIUM;
-  public HARD = HARD;
+  public BEGINNER = BEGINNER;
+  public INTERMEDIATE = INTERMEDIATE;
+  public ADVANCED = ADVANCED;
+  public EXPERT = EXPERT;
+
   public difficult: string;
 
-  private interval;
+  private startGameSubscription: Subscription;
+  private timerSubscription: Subscription;
 
-  constructor() {
+  constructor(private gameService: GameService) {
   }
 
-  ngOnInit() {
-    this.difficult = EASY;
+  ngOnInit(): void {
+    this.initializeGameDifficult();
+    this.timerSubscription = this.gameService.timer.subscribe(
+      (time: number) => {
+        this.time = time;
+      }
+    );
+  }
+
+  onStartNewGame(): void {
+    this.time = 0;
+    this.difficult = this.selectDifficultFormGroup.get('difficult').value;
+    this.gameService.startGame();
+  }
+
+
+  private initializeGameDifficult(): void {
+    this.difficult = BEGINNER;
     this.selectDifficultFormGroup = new FormGroup({
-      'difficult': new FormControl(EASY, Validators.required),
+      difficult: new FormControl(BEGINNER, Validators.required),
     });
   }
 
-  onStartNewGame() {
-    this.difficult = this.selectDifficultFormGroup.get('difficult').value;
-    if (this.interval) {
-      clearInterval(this.interval);
-      this.timer = 0;
-    }
-    this.interval = setInterval(() => {
-      this.timer++;
-    }, 1000);
+  ngOnDestroy(): void {
+    this.startGameSubscription.unsubscribe();
   }
 }
