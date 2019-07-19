59bb84d66f82d3a2b07e8a4adfcd61e0870f29f3
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-19 11:53:45 +0400

    [project] Add flag to constants.

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,4 +2,9 @@ export const BEGINNER = 'beginner';
 export const INTERMEDIATE = 'intermediate';
 export const ADVANCED = 'advanced';
 export const EXPERT = 'expert';
-
+export const FLAG_MINE = 'mine';
+export const FLAG_UNDEFINED = 'undefined';
+export const FLAGS = [
+  FLAG_MINE,
+  FLAG_UNDEFINED
+];
