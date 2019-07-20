294ac7da56a8d051b688e46ddfdbadc196407149
Elmir Ismayilzade <kryternext@smartup.school>
2019-07-20 15:48:41 +0400

    [feature] Add difficulty level to constants

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,6 +2,12 @@ export const BEGINNER = 'beginner';
 export const INTERMEDIATE = 'intermediate';
 export const ADVANCED = 'advanced';
 export const EXPERT = 'expert';
+export const DIFFICULTY_LEVELS = [
+  {level: BEGINNER, width: 9, height: 9, minesCount: 10},
+  {level: INTERMEDIATE, width: 12, height: 12, minesCount: 25},
+  {level: ADVANCED, width: 16, height: 16, minesCount: 40},
+  {level: EXPERT, width: 30, height: 16, minesCount: 99},
+];
 export const FLAG_MINE = 'mine';
 export const FLAG_UNDEFINED = 'undefined';
 export const FLAGS = [
